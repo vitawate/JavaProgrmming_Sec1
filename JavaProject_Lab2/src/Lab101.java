afdeebdc0ccf8f1003358699604a6dc5b586675c
Vitawat Kongjun <noahzxcv99@gmail.com>
2023-11-21 11:55:11 +0700

    Attendnce(21-11-2023)

---
+++ JavaProject_Lab2/src/Lab101.java
@@ -0,0 +1,24 @@
+import java.text.DecimalFormat;
+import java.util.*;;
+public class Lab101{
+	public static void main(String[] args) {
+		DecimalFormat frm = new DecimalFormat("#,###.00");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input product name : ");
+		String productName = input.nextLine() ;
+		System.out.print("Input product unit : ");
+		int productUnit = input.nextInt();
+		System.out.print("Input price per unit : ");
+		float priceperUnit = input.nextFloat();
+		System.out.println();
+		System.out.println("------------------------------------------------------------");
+		double totalPrice = productUnit *productPriseper
+		System.out.print("Input price is : "+frm.format(totalPrice));
+		float totalPrice = productUnit * priceperUnit;
+		System.out.println("Total Price is "+totalPrice + " baht.");
+		
+		
+		double serviceCharge = totalPrice + (totalPrice*7/100);
+		System.out.println("Add VAT 7% is "+frm.format(serviceCharge));
+
+}
